9b6161fa1b7aaa4e685ca8dbdf71d68b3e781e26
alexcologia33 <alexcologia33@gmail.com>
2019-11-09 20:29:27 -0200

    dev

--- layout/scripts.php
+++ layout/scripts.php
@@ -1,13 +1,13 @@
   <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/vendor/jquery/jquery.min.js"></script>
+  <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Plugin JavaScript -->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Contact form JavaScript -->
-  <script src="js/jqBootstrapValidation.js"></script>
-  <script src="js/contato.js"></script>
+  <script src="/js/jqBootstrapValidation.js"></script>
+  <script src="/js/contato.js"></script>
 
   <!-- Custom scripts for this template -->
-  <script src="js/main.min.js"></script>
+  <script src="/js/main.min.js"></script>
