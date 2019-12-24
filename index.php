6c231c64435eebde5349f11aa2e28a1dc2cf872f
javaboybr <alexcologia33@gmail.com>
2019-12-24 02:09:29 -0300

    dev

--- index.php
+++ index.php
@@ -138,7 +138,7 @@ position: relative;
   <div class="w3-text-white" style="padding:48px">
     <span class="w3-jumbo w3-hide-small">Móveis Sob Medida</span><br>
     <span class="w3-xxlarge w3-hide-large w3-hide-medium">Móveis Sob Medida</span><br>
-    <p><a href="/projetos" class="w3-button w3-white w3-padding-large w3-large w3-margin-top 
+    <p><a href="/#portfolio" class="w3-button w3-white w3-padding-large w3-large w3-margin-top 
         w3-opacity w3-hover-opacity-off">Confira o nosso portfólio</a></p>
   </div> 
   <div class="w3-display-bottomleft text-y" style="padding:24px 48px;font-size: 3em;">
